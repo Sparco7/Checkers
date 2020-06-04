b0efd3e1824df17df92bf8b5bec8a93949c25ad2
Sparco7 <barakbbg@gmail.com>
2020-06-04 18:09:54 +0300

    Updates

---
+++ precache-manifest.288fe687037b37a9d2f8eb8181e912d0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0490308b3c794c471976c15b57cb38e",
+    "url": "/Checkers/index.html"
+  },
+  {
+    "revision": "3081e4e6373d368d41bd",
+    "url": "/Checkers/static/css/main.bb933895.chunk.css"
+  },
+  {
+    "revision": "17376ab014d444045909",
+    "url": "/Checkers/static/js/2.53dd330c.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/Checkers/static/js/2.53dd330c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3081e4e6373d368d41bd",
+    "url": "/Checkers/static/js/main.02d4b23e.chunk.js"
+  },
+  {
+    "revision": "9767fc0e8928793c6f60",
+    "url": "/Checkers/static/js/runtime-main.c4f2d067.js"
+  }
+]);
