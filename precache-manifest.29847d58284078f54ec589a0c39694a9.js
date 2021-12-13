a75812c7bf22fdf8803dd94ffab7cea43ffd0f8a
Barak Ben Gur <barakbbg@gmail.com>
2021-12-13 16:40:13 +0200

    Updates

---
+++ precache-manifest.29847d58284078f54ec589a0c39694a9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a468ff706dd2bd87c54d492dda116b13",
+    "url": "/Checkers/index.html"
+  },
+  {
+    "revision": "6ba2f40d74f9384d801c",
+    "url": "/Checkers/static/css/main.e5b3b4d2.chunk.css"
+  },
+  {
+    "revision": "33c3fdb4bf428905be32",
+    "url": "/Checkers/static/js/2.f0c3e282.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/Checkers/static/js/2.f0c3e282.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6ba2f40d74f9384d801c",
+    "url": "/Checkers/static/js/main.a5e765bb.chunk.js"
+  },
+  {
+    "revision": "9767fc0e8928793c6f60",
+    "url": "/Checkers/static/js/runtime-main.c4f2d067.js"
+  }
+]);
